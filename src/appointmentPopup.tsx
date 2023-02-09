e3069ced6f225d7c1cc096e61fe03f3cd7db7cad
Hayley <haysho2260@gmail.com>
2023-02-08 17:06:10 -0800

    style text inside textbox

--- src/appointmentPopup.tsx
+++ src/appointmentPopup.tsx
@@ -11,10 +11,19 @@ const Button = styled.button`
   border: none;
   color: white;
 `;
+const Div = styled.div`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+`;
 const StyledPopup = styled(Popup)`
   border: none;
+  display: flex;
+  shadow: none;
+  background: black;
 `;
-const Section = styled.div`
+const RiderInfo = styled(Div)`
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -24,21 +33,30 @@ const Section = styled.div`
   font-size: 80%;
   line-height: 19px;
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: black;
+  background: white;
+  padding: 2.5%;
+`;
+const Header = styled(Div)`
+  padding: 10%;
+  font-family: "Roboto";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+  background: white;
+  color: #1b4c5a;
 `;
 
 export default function AppointmentPopup() {
   return (
-    <Section>
-      <Section>
-        <StyledPopup
-          trigger={<Button> Trigger</Button>}
-          position="right center"
-        >
-          <Section className="infoPopup">
-            <Horse /> Riders: Jane Doe, John Smith
-          </Section>
-        </StyledPopup>
-      </Section>
-    </Section>
+    <div>
+      <StyledPopup trigger={<Button> Trigger</Button>} position="right center">
+        <Header>Appointment Info</Header>
+        <RiderInfo>
+          <Horse /> Riders: Jane Doe, John Smith
+        </RiderInfo>
+      </StyledPopup>
+    </div>
   );
 }
