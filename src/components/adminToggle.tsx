4cd93b14d922b30d0a3bd97d4da41405bc8692a1
Parshana007 <parshana007@gmail.com>
2023-02-25 15:55:20 -0800

    fix: stylzed components to coloring/theme

--- src/components/adminToggle.tsx
+++ src/components/adminToggle.tsx
@@ -2,26 +2,48 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import { Wrapper, Box, Label, Description } from "./styledComponents";
 
+const Encompasing = styled(Wrapper)``;
+
 const CheckBox = styled.div`
+  border-radius: 5px;
   display: flex;
   align-items: center;
   justify-content: center;
-  border: solid 0.5px #c4c4c4;
-  padding: 10%;
+  border: solid 2px #1b4c5a;
+  padding: 3%;
   width: 5%;
 `;
 const Check = styled.div`
+  border-radius: 5px;
   border: solid 0.5px #c4c4c4;
-  background-color: red;
-  height: 50%;
-  width: 50%;
-  padding: 50%;
+  background-color: #1b4c5a;
+  height: 10%;
+  width: 10%;
+  padding: 40%;
 `;
 const Row = styled.div`
-  border: solid 0.5px #c4c4c4;
   display: flex;
   flex-direction: row;
+  padding: 3%;
 `;
+
+const ViewingBox = styled(Box)`
+  border: 0;
+  box-shadow: none;
+`;
+
+const ViewingText = styled(Label)`
+  padding-bottom: 10%;
+  font-size: 130%;
+  font-family: Roboto;
+`;
+
+const ViewingDescription = styled(Description)`
+  font-family: Roboto;
+  padding-left: 3%;
+  font-size: 100%;
+`;
+
 export default function AdminToggle() {
   // Initialize a boolean state
   const [showVolunteers, setShowVolunteers] = useState(false);
@@ -33,7 +55,6 @@ export default function AdminToggle() {
     // inverse the boolean state of passwordShown
     setShowVolunteers(!showVolunteers);
     setShowRiders(false);
-    setShowVolunteers(!showVolunteers);
     setShowBoth(false);
   };
   const toggleRider = () => {
@@ -46,32 +67,32 @@ export default function AdminToggle() {
   const toggleBoth = () => {
     // When the handler is invoked
     // inverse the boolean state of passwordShown
+    setShowBoth(!showBoth);
     setShowRiders(false);
     setShowVolunteers(false);
-    setShowBoth(!showBoth);
   };
 
   return (
-    <Wrapper>
-      <Box>
+    <Encompasing>
+      <ViewingBox>
         {(showVolunteers || showBoth) && <div>show volunteer times</div>}
         {(showRiders || showBoth) && <div>show rider times</div>}
-        <Label>Viewing:</Label>
+        <ViewingText>Viewing:</ViewingText>
+        <Row>
+          <CheckBox onClick={toggleBoth}>{showBoth && <Check />}</CheckBox>
+          <ViewingDescription>Both</ViewingDescription>
+        </Row>
         <Row>
           <CheckBox onClick={toggleVolunteer}>
             {showVolunteers && <Check />}
           </CheckBox>
-          <Description>Volunteer</Description>
+          <ViewingDescription>Volunteer only</ViewingDescription>
         </Row>
         <Row>
           <CheckBox onClick={toggleRider}>{showRiders && <Check />}</CheckBox>
-          <Description>Rider</Description>
+          <ViewingDescription>Rider only</ViewingDescription>
         </Row>
-        <Row>
-          <CheckBox onClick={toggleBoth}>{showBoth && <Check />}</CheckBox>
-          <Description>Both</Description>
-        </Row>
-      </Box>
-    </Wrapper>
+      </ViewingBox>
+    </Encompasing>
   );
 }
