e92f12aae8125ce053a1dbf6f1054b6de519f163
Hayley <haysho2260@gmail.com>
2023-02-27 17:25:34 -0800

    hardcode time slots

---
+++ src/components/timeslot.tsx
@@ -0,0 +1,114 @@
+import React, { ChangeEvent, useState } from "react";
+import styled from "styled-components";
+import logoPic from "../images/PETlogo.jpg";
+import eyeSlash from "../images/eyeSlash.svg";
+import {
+  Wrapper,
+  Box,
+  Button,
+  Input,
+  Label,
+  PasswordContainer,
+  EyeSlash,
+  Question,
+  TextLink,
+  ErrorMessage,
+} from "./styledComponents";
+
+const Logo = styled.img`
+  display: flex;
+  margin: auto;
+  /* PET_FINAL logo 1 */
+  width: 150px;
+`;
+
+function addAccount() {
+  alert("You clicked login");
+}
+
+export default function Timeslot() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  // Initialize a boolean state
+  const [passwordShown, setPasswordShown] = useState(false);
+  // Password toggle handler
+  const togglePassword = () => {
+    // When the handler is invoked
+    // inverse the boolean state of passwordShown
+    setPasswordShown(!passwordShown);
+  };
+  const [validEmail, setValidEmail] = React.useState(false);
+  const handleOnChangeEmail = (email1: string) => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(email1)) {
+      setValidEmail(false);
+    } else {
+      setValidEmail(true);
+    }
+  };
+
+  return (
+    <Wrapper>
+      <Box>
+        <Logo src={logoPic} alt="PET logo" />
+        {validEmail && (
+          <ErrorMessage>Invalid email. Please try again.</ErrorMessage>
+        )}
+        <Label>Email</Label>
+        <Input
+          placeholder=""
+          type="email"
+          value={email} // add newEmail as the input's value
+          onChange={(e: ChangeEvent<HTMLInputElement>) => {
+            setEmail(e.target.value);
+          }}
+          onClick={(e) => {
+            e.currentTarget.scrollLeft = e.currentTarget.scrollWidth;
+            e.currentTarget.setSelectionRange(
+              e.currentTarget.value.length,
+              e.currentTarget.value.length
+            );
+          }}
+          onBlur={() => {
+            handleOnChangeEmail(email);
+          }}
+        />
+        <Label>Password</Label>
+        <PasswordContainer>
+          <EyeSlash onClick={togglePassword}>
+            <img src={eyeSlash} alt="didn't work" />
+          </EyeSlash>
+          <Input
+            type={passwordShown ? "text" : "password"}
+            value={password}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setPassword(e.target.value);
+            }}
+            onFocus={(e) => {
+              e.currentTarget.parentElement?.classList.remove("inputBlur");
+              e.currentTarget.parentElement?.classList.add("inputFocus");
+            }}
+            onBlur={(e) => {
+              e.currentTarget.parentElement?.classList.remove("inputFocus");
+              e.currentTarget.parentElement?.classList.add("inputBlur");
+            }}
+            onClick={(e) => {
+              e.currentTarget.scrollLeft = e.currentTarget.scrollWidth;
+              e.currentTarget.setSelectionRange(
+                e.currentTarget.value.length,
+                e.currentTarget.value.length
+              );
+            }}
+          />
+        </PasswordContainer>
+        <TextLink to="/forgot-password">Forgot password?</TextLink>
+        <Button onClick={addAccount}>Log In</Button>
+        <Question>
+          Don&apos;t have an account?&nbsp;
+          <TextLink to="/create-account">Create Account</TextLink>
+        </Question>
+      </Box>
+    </Wrapper>
+  );
+}
---
+++ src/components/timeslots.tsx
@@ -0,0 +1,23 @@
+export interface Timeslots {
+  time: string;
+  riders: string[];
+  volunteers: string[];
+  status: boolean;
+}
+export var timeslot: Timeslots[] = [
+  { time: "9:00am - 9:30am", riders: [], volunteers: [], status: false },
+  { time: "9:30am - 10:00am", riders: [], volunteers: [], status: false },
+  { time: "10:00am - 10:30am", riders: [], volunteers: [], status: false },
+  { time: "10:35am - 11:05am", riders: [], volunteers: [], status: false },
+  { time: "11:10am - 11:40am", riders: [], volunteers: [], status: false },
+  { time: "11:45am - 12:15pm", riders: [], volunteers: [], status: false },
+  { time: "12:20pm - 12:50pm", riders: [], volunteers: [], status: false },
+  { time: "12:55pm - 1:25pm", riders: [], volunteers: [], status: false },
+  { time: "1:30pm - 2:00pm", riders: [], volunteers: [], status: false },
+  { time: "2:00pm - 2:30pm", riders: [], volunteers: [], status: false },
+  { time: "2:30pm - 3:00pm", riders: [], volunteers: [], status: false },
+  { time: "3:00pm - 3:30pm", riders: [], volunteers: [], status: false },
+  { time: "3:30pm - 4:00pm", riders: [], volunteers: [], status: false },
+  { time: "4:00pm - 4:30pm", riders: [], volunteers: [], status: false },
+  { time: "4:30pm - 5:00pm", riders: [], volunteers: [], status: false },
+];
