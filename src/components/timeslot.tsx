6d7a25a90ec66d8b3b028d14898668404fa23626
hayley <haysho2260@gmail.com>
2023-03-06 15:10:02 -0800

    make drop down arrow

--- src/components/timeslot.tsx
+++ src/components/timeslot.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
 import timeslots from "./timeslots";
+import chevronLeft from "../images/chevronLeft.svg";
 
 import {
   Wrapper,
@@ -14,12 +16,49 @@ import {
   // ErrorMessage,
 } from "./styledComponents";
 
+const ChevronDown = styled.img`
+  width: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  transform: rotate(270deg);
+`;
+const ChevronUp = styled.img`
+  width: auto;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  transform: rotate(90deg);
+`;
+const StyledBtn = styled.button`
+  border: none;
+  background: none;
+  vertical-align: middle;
+`;
 export default function Timeslot() {
+  const [dropdownShown, setDropdownShown] = useState(false);
+
+  const handleDropDown = () => {
+    setDropdownShown(!dropdownShown);
+  };
   return (
     <Wrapper>
       <Box>
         {timeslots.map((time) => (
-          <Box> {time} </Box>
+          <Box>
+            {" "}
+            {time}{" "}
+            {!dropdownShown && (
+              <StyledBtn type="button" onClick={handleDropDown}>
+                <ChevronUp src={chevronLeft} />
+              </StyledBtn>
+            )}
+            {dropdownShown && (
+              <StyledBtn type="button" onClick={handleDropDown}>
+                <ChevronDown src={chevronLeft} />
+              </StyledBtn>
+            )}
+          </Box>
         ))}
       </Box>
     </Wrapper>
