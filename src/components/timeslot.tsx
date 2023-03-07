e8599e3185d266aa5b261f45f702fbf595e650c3
hayley <haysho2260@gmail.com>
2023-03-06 18:09:53 -0800

    hard coded individual checks

--- src/components/timeslot.tsx
+++ src/components/timeslot.tsx
@@ -43,27 +43,133 @@ const Boxed = styled(Box)``;
 const ButtonToggle = styled(Button)``;
 
 export default function Timeslot() {
-  // const [dropdownShown0, setDropdownShown0] = useState(false);
-  const [showChecked, setshowChecked] = useState(false);
-
-  // const [showUnchecked, setshowUnchecked] = useState(false);
-
-  const toggleChecked = () => {
-    // When the handler is invoked
-    // inverse the boolean state of passwordShown
-    setshowChecked(!showChecked);
-    // setshowUnchecked(false);
+  const [showChecked0, setshowChecked0] = useState(false);
+  const toggleChecked0 = () => {
+    setshowChecked0(!showChecked0);
   };
-  // const toggleUnchecked = () => {
-  //   // When the handler is invoked
-  //   // inverse the boolean state of passwordShown
-  //   setshowUnchecked(!showUnchecked);
-  //   setshowChecked(false);
-  // };
+  const [showChecked1, setshowChecked1] = useState(false);
+  const toggleChecked1 = () => {
+    setshowChecked1(!showChecked1);
+  };
+  const [showChecked2, setshowChecked2] = useState(false);
+  const toggleChecked2 = () => {
+    setshowChecked2(!showChecked2);
+  };
+  const [showChecked3, setshowChecked3] = useState(false);
+  const toggleChecked3 = () => {
+    setshowChecked3(!showChecked3);
+  };
+  const [showChecked4, setshowChecked4] = useState(false);
+  const toggleChecked4 = () => {
+    setshowChecked4(!showChecked4);
+  };
+  const [showChecked5, setshowChecked5] = useState(false);
+  const toggleChecked5 = () => {
+    setshowChecked5(!showChecked5);
+  };
+  const [showChecked6, setshowChecked6] = useState(false);
+  const toggleChecked6 = () => {
+    setshowChecked6(!showChecked6);
+  };
+  const [showChecked7, setshowChecked7] = useState(false);
+  const toggleChecked7 = () => {
+    setshowChecked7(!showChecked7);
+  };
+  const [showChecked8, setshowChecked8] = useState(false);
+  const toggleChecked8 = () => {
+    setshowChecked8(!showChecked8);
+  };
+  const [showChecked9, setshowChecked9] = useState(false);
+  const toggleChecked9 = () => {
+    setshowChecked9(!showChecked9);
+  };
 
   return (
     <Wrapper>
       <Boxed>
+        <Box>{timeslots[0]}</Box>
+        <ButtonToggle onClick={toggleChecked0}>
+          {showChecked0 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[1]}</Box>
+        <ButtonToggle onClick={toggleChecked1}>
+          {showChecked1 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[2]}</Box>
+        <ButtonToggle onClick={toggleChecked2}>
+          {showChecked2 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[3]}</Box>
+        <ButtonToggle onClick={toggleChecked3}>
+          {showChecked3 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[4]}</Box>
+        <ButtonToggle onClick={toggleChecked4}>
+          {showChecked4 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[5]}</Box>
+        <ButtonToggle onClick={toggleChecked5}>
+          {showChecked5 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[6]}</Box>
+        <ButtonToggle onClick={toggleChecked6}>
+          {showChecked6 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[7]}</Box>
+        <ButtonToggle onClick={toggleChecked7}>
+          {showChecked7 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[8]}</Box>
+        <ButtonToggle onClick={toggleChecked8}>
+          {showChecked8 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+        <Box>{timeslots[9]}</Box>
+        <ButtonToggle onClick={toggleChecked9}>
+          {showChecked9 ? (
+            <img src={Checked} alt="Checked Img" />
+          ) : (
+            <img src={Unchecked} alt="Unchecked Img" />
+          )}
+        </ButtonToggle>
+      </Boxed>
+
+      {/* <Boxed>
         {timeslots.map((time) => (
           <>
             <Box>{time}</Box>
@@ -76,7 +182,7 @@ export default function Timeslot() {
             </ButtonToggle>
           </>
         ))}
-      </Boxed>
+      </Boxed> */}
     </Wrapper>
   );
 }
