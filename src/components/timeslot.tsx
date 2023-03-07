fa8089c43989ef81c55db8986e8da3956e3ee931
Parshana007 <parshana007@gmail.com>
2023-03-06 18:27:43 -0800

    changes have been made

--- src/components/timeslot.tsx
+++ src/components/timeslot.tsx
@@ -44,37 +44,88 @@ const ButtonToggle = styled(Button)``;
 
 export default function Timeslot() {
   // const [dropdownShown0, setDropdownShown0] = useState(false);
-  const [showChecked, setshowChecked] = useState(false);
+  // const [showChecked, setshowChecked] = useState(false);
+  // // const [showUnchecked, setshowUnchecked] = useState(false);
 
-  // const [showUnchecked, setshowUnchecked] = useState(false);
-
-  const toggleChecked = () => {
-    // When the handler is invoked
-    // inverse the boolean state of passwordShown
-    setshowChecked(!showChecked);
-    // setshowUnchecked(false);
-  };
+  // const toggleChecked = () => {
+  //   // When the handler is invoked
+  //   // inverse the boolean state of passwordShown
+  //   setshowChecked(!showChecked);
+  //   // setshowUnchecked(false);
+  // };
   // const toggleUnchecked = () => {
   //   // When the handler is invoked
   //   // inverse the boolean state of passwordShown
   //   setshowUnchecked(!showUnchecked);
   //   setshowChecked(false);
   // };
+  const [dropdownShown0, setDropdownShown0] = useState(false);
+
+  const handleDropDown0 = () => {
+    setDropdownShown0(!dropdownShown0);
+  };
+  // const [dropdownShown1, setDropdownShown1] = useState(false);
+
+  // const handleDropDown1 = () => {
+  //   setDropdownShown1(!dropdownShown1);
+  // };
+  // const [dropdownShown2, setDropdownShown2] = useState(false);
+
+  // const handleDropDown2 = () => {
+  //   setDropdownShown2(!dropdownShown2);
+  // };
+  // const [dropdownShown3, setDropdownShown3] = useState(false);
+
+  // const handleDropDown3 = () => {
+  //   setDropdownShown3(!dropdownShown3);
+  // };
+  // const [dropdownShown4, setDropdownShown4] = useState(false);
+
+  // const handleDropDown4 = () => {
+  //   setDropdownShown4(!dropdownShown4);
+  // };
+  // const [dropdownShown5, setDropdownShown5] = useState(false);
+
+  // const handleDropDown5 = () => {
+  //   setDropdownShown5(!dropdownShown5);
+  // };
+  // const [dropdownShown6, setDropdownShown6] = useState(false);
+
+  // const handleDropDown6 = () => {
+  //   setDropdownShown6(!dropdownShown6);
+  // };
+  // const [dropdownShown7, setDropdownShown7] = useState(false);
+
+  // const handleDropDown7 = () => {
+  //   setDropdownShown7(!dropdownShown7);
+
+  //   const [dropdownShown8, setDropdownShown8] = useState(false);
+
+  //   const handleDropDown8 = () => {
+  //     setDropdownShown8(!dropdownShown8);
+  //   };
+  //   const [dropdownShown9, setDropdownShown9] = useState(false);
+
+  //   const handleDropDown9 = () => {
+  //     setDropdownShown9(!dropdownShown9);
+  //   };
 
   return (
     <Wrapper>
       <Boxed>
-        {timeslots.map((time) => (
-          <>
-            <Box>{time}</Box>
-            <ButtonToggle onClick={toggleChecked}>
-              {showChecked ? (
+        {timeslots.map(item => (
+          <Box>
+            const container = {};
+
+            container[item.time]
+            <ButtonToggle onClick={handleDropDown0}>
+              {dropdownShown0 ? (
                 <img src={Checked} alt="Checked Img" />
               ) : (
                 <img src={Unchecked} alt="Unchecked Img" />
               )}
             </ButtonToggle>
-          </>
+          </Box>
         ))}
       </Boxed>
     </Wrapper>
