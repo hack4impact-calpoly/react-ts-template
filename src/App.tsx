8363105324c8db9e72b074017c94d0ea7b407700
borajoo <chocobora1@gmail.com>
2023-03-09 11:40:26 -0800

    Fix route

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import CreateAccount from "./components/createAccount";
 import EnterCode from "./components/enterCode";
 import Login from "./components/login";
 import ForgotPassword from "./components/forgotPassword";
-import WeeklyView from "./components/weeklyView";
 import Calendar from "./components/calendar";
 
 Amplify.configure(awsconfig);
@@ -21,7 +20,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* /, /login, /create-account, /forgot-password, /enter-code, /reset-password, /success */}
-        <Route path="/" element={<WeeklyView startDate={new Date()} />} />
+        <Route path="/" element={<Calendar />} />
         <Route path="/login" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/enter-code" element={<EnterCode />} />
@@ -34,7 +33,6 @@ function App() {
           element={<ResetPassword email={email!} />}
         />
         <Route path="/success/:id" element={<Success />} />
-        <Route path="/calendar" element={<Calendar />} />
       </Routes>
     </BrowserRouter>
   );
