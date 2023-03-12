132adac809e5b2cebf6d46932709ba0280b01a03
Parshana007 <parshana007@gmail.com>
2023-03-12 00:10:28 -0800

    Changed times

--- src/components/timeslots.tsx
+++ src/components/timeslots.tsx
@@ -1,22 +1,22 @@
 const timeslots = [
   {
     startTime: new Date(2023, 2, 7, 9, 0),
+    endTime: new Date(2023, 2, 7, 10, 30),
+    checked: false,
+  },
+  {
+    startTime: new Date(2023, 2, 7, 9, 30),
     endTime: new Date(2023, 2, 7, 10, 0),
     checked: false,
   },
   {
     startTime: new Date(2023, 2, 7, 10, 0),
-    endTime: new Date(2023, 2, 7, 11, 0),
+    endTime: new Date(2023, 2, 7, 10, 30),
     checked: false,
   },
   {
-    startTime: new Date(2023, 2, 7, 11, 0),
-    endTime: new Date(2023, 2, 7, 12, 0),
-    checked: false,
-  },
-  {
-    startTime: new Date(2023, 2, 7, 12, 0),
-    endTime: new Date(2023, 2, 7, 13, 0),
+    startTime: new Date(2023, 2, 7, 10, 35),
+    endTime: new Date(2023, 2, 7, 11, 5),
     checked: false,
   },
   {
