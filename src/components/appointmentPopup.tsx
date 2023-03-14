d54ba845e87161ffbb3d40c69625ad1c869a680b
hayley <haysho2260@gmail.com>
2023-03-13 19:45:27 -0700

    delete unnecessary downloads

--- src/components/appointmentPopup.tsx
+++ src/components/appointmentPopup.tsx
@@ -5,8 +5,6 @@ import styled from "styled-components";
 import { Box } from "./styledComponents";
 import AppointmentInfo from "./appointmentInfo";
 
-// import TimeSlotConfirmation from "./timeSlotConfirmation";
-
 const Header = styled.h1`
   font-family: "Roboto";
   font-style: normal;
@@ -27,22 +25,23 @@ const ButtonPop = styled.button`
   align-self: center;
   cursor: pointer;
 `;
-
-const useStyles = makeStyles({
-  root: {
-    background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    border: 0,
-    borderRadius: 3,
-    boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
-    color: "white",
-    height: 48,
-    padding: "0 30px",
-  },
-});
+const ConfirmButton = styled(Link)`
+  background: #1b4c5a;
+  border: solid 0.5px #6c6b6b;
+  color: white;
+  height: 2.8rem;
+  margin-top: 2rem;
+  font-weight: bold;
+  width: 15%;
+  align-self: center;
+  cursor: pointer;
+`;
+const ButtonText = styled.text`
+  display: none;
+`;
 
 export default function AppointmentPopup() {
   const [popupShown, setPopupShown] = useState(false);
-  const classes = useStyles();
   const togglePopup = () => {
     setPopupShown(!popupShown);
   };
@@ -54,14 +53,9 @@ export default function AppointmentPopup() {
       <Box style={{ display: popupShown ? "block" : "none" }}>
         <Header>Appointment Info</Header>
         <AppointmentInfo />
-        <Button
-          type="button"
-          component={Link}
-          to="/time-slot-confirmation"
-          className={classes.root}
-        >
-          Click Me
-        </Button>
+        <ConfirmButton type="button" to="/time-slot-confirmation">
+          <ButtonText>Confirm</ButtonText>
+        </ConfirmButton>
       </Box>
     </div>
   );
