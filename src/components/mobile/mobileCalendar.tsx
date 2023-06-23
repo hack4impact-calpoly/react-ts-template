598d92b7432a4c4c2d75d173281a5a06a2462567
Sam Wathen <sfwathen@calpoly.edu>
2023-06-22 19:08:11 -0700

    styling fixes on mobile calendar, scrolling still possible

--- src/components/mobile/mobileCalendar.tsx
+++ src/components/mobile/mobileCalendar.tsx
@@ -8,6 +8,7 @@ import MobileWeeklyView from "./mobileWeeklyView";
 import { LazyTimeslot } from "../../models";
 import { Dropdown, Option } from "./dropdown";
 import signoutarrow from "../../images/SignOutArrow.png";
+import { Wrapper } from "../styledComponents";
 
 const CurrentDate = styled.p`
   font-family: "Rubik";
@@ -19,17 +20,60 @@ const CurrentDate = styled.p`
   color: #1b4c5a;
   display: flex;
   align-items: center;
-  margin-top: 20%;
+  // height: 30px;
+  // margin-top: 20%;
   // padding-top: 10%;
 `;
 
+const Box = styled.section`
+  border: solid 0.5px #c4c4c4;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  justify-items: center;
+  font-family: "Rubik", sans-serif;
+  background: white;
+  width: 35%;
+  // padding: 3rem 5rem;
+
+  @media (max-width: 500px) {
+    border: none;
+    box-shadow: none;
+    display: flex;
+    margin-top: 10%;
+    padding: 0;
+    width: 100%;
+  }
+`;
+
+const Header1 = styled.text`
+  display: none;
+  @media (max-width: 500px) {
+    padding-top: 25px;
+    display: flex;
+    align-self: left;
+    font-size: 140%;
+    padding-bottom: 8%;
+    color: #1b4c5a;
+  }
+`;
+
 const StyledButton = styled(Link)`
-  display: inline-block;
-  position: absolute;
-  right: 0;
   width: 80px;
   height: 80px;
   transform: scale(0.5);
+`;
+
+const HorizontalFlex = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const InvisibleButton = styled(Link)`
+  width: 80px;
+  height: 80px;
+  visibility: hidden;
 `;
 
 const StyledImage = styled.img`
@@ -61,10 +105,6 @@ export default function CalendarMobile({ timeslots }: CalendarProps) {
   const [realUser] = currentUser;
   const { userType } = realUser;
   const [toggleValue, setToggleValue] = useState("");
-  // these values are hardcoded for conditional rendering of showing different slots
-  // eslint-disable-next-line no-param-reassign
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  // bookingsFake = 1;
 
   // this is to create the current selected date string
   const currentTimeString: string[] = [];
@@ -91,47 +131,55 @@ export default function CalendarMobile({ timeslots }: CalendarProps) {
   };
 
   return (
-    <div>
-      <StyledButton to="/logout">
-        <StyledImage src={signoutarrow} alt="Sign Out" />
-      </StyledButton>
+    <Wrapper>
+      <Box>
+        <HorizontalFlex>
+          <InvisibleButton to="/logout" />
+          <Header1>Schedule</Header1>
+          <StyledButton to="/logout">
+            <StyledImage src={signoutarrow} alt="Sign Out" />
+          </StyledButton>
+        </HorizontalFlex>
 
-      {/* renders the calendar  */}
-      <MobileWeeklyView
-        currentDate={currentDate}
-        setCurrentDate={setCurrentDate}
-        setDayProp={setDayProp}
-        setMonthProp={setMonthProp}
-        setWeekdayProp={setWeekdayProp}
-      />
-
-      {/* creates the current selected date */}
-      <CurrentDate>{currentTimeString}</CurrentDate>
-
-      {/* this is for the toggle dropdown with different options on different user types */}
-      <Dropdown onChange={handleSelect}>
-        <Option
-          selected
-          value={userType === "Admin" ? "Both" : "Availability"}
-        />
-        <Option
-          value={userType !== "Admin" ? "My Slots" : "Riders"}
-          selected={undefined}
+        {/* renders the calendar  */}
+        <MobileWeeklyView
+          currentDate={currentDate}
+          setCurrentDate={setCurrentDate}
+          setDayProp={setDayProp}
+          setMonthProp={setMonthProp}
+          setWeekdayProp={setWeekdayProp}
         />
 
-        {userType === "Admin" ? (
-          <Option value="Volunteers" selected={undefined} />
-        ) : (
-          <div>0</div>
-        )}
-      </Dropdown>
+        <HorizontalFlex>
+          {/* creates the current selected date */}
+          <CurrentDate>{currentTimeString}</CurrentDate>
 
-      {/* the timeslots will change depending on the usertype */}
-      <MobileTimeslots
-        models={timeslots}
-        date={currentDate}
-        toggleValue={toggleValue}
-      />
-    </div>
+          {/* this is for the toggle dropdown with different options on different user types */}
+          <Dropdown onChange={handleSelect}>
+            <Option
+              selected
+              value={userType === "Admin" ? "Both" : "Availability"}
+            />
+            <Option
+              value={userType !== "Admin" ? "My Slots" : "Riders"}
+              selected={undefined}
+            />
+
+            {userType === "Admin" ? (
+              <Option value="Volunteers" selected={undefined} />
+            ) : (
+              <div>0</div>
+            )}
+          </Dropdown>
+        </HorizontalFlex>
+
+        {/* the timeslots will change depending on the usertype */}
+        <MobileTimeslots
+          models={timeslots}
+          date={currentDate}
+          toggleValue={toggleValue}
+        />
+      </Box>
+    </Wrapper>
   );
 }
